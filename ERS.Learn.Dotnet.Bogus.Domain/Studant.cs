aa10391356b7cdef147b6bcf3a25176c21cf8988
Elton Souza <eltonricsouza@gmail.com>
2022-10-12 18:04:56 -0300

    feat(project): initial commit

---
+++ ERS.Learn.Dotnet.Bogus.Domain/Studant.cs
@@ -0,0 +1,15 @@
+﻿namespace ERS.Learn.Dotnet.Bogus.Domain
+{
+    public class Studant
+    {
+        public int Name { get; set; }
+        public DateTime DateBorn { get; set; }
+        public bool Enable { get; set; }
+        public Guid RA { get; set; }
+
+        public Studant()
+        {
+            RA = Guid.NewGuid();
+        }
+    }
+}
